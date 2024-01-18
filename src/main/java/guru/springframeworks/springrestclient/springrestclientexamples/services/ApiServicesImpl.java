fa443e67e7d5226c27da2381ce97890de3126aec
bbqboneless <sammylicea5@gmail.com>
2024-01-18 11:43:52 -0600

    Improved api uri

--- src/main/java/guru/springframeworks/springrestclient/springrestclientexamples/services/ApiServicesImpl.java
+++ src/main/java/guru/springframeworks/springrestclient/springrestclientexamples/services/ApiServicesImpl.java
@@ -1,19 +1,27 @@
 package guru.springframeworks.springrestclient.springrestclientexamples.services;
 
 import guru.springframeworks.springrestclient.api.domain.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 @Service
 public class ApiServicesImpl implements ApiServices{
     private RestTemplate restTemplate;
-    public ApiServicesImpl(RestTemplate restTemplate){
+    private final String api_url;
+
+    public ApiServicesImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
         this.restTemplate = restTemplate;
+        this.api_url = api_url;
     }
+
     @Override
     public List<User> getUsers(Integer limit) {
-        List<User> userData = restTemplate.getForObject("https://jsonplaceholder.typicode.com/users?_limit=" + limit, List.class);
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(api_url)
+                .queryParam("limit",limit);
+        List<User> userData = restTemplate.getForObject(uriBuilder.toUriString(), List.class);
         return userData;
     }
 }
